786aabaae51af0eec80def927baecbe4762e5f1d
xushengG <gxsheng89@163.com>
2024-05-23 13:47:20 +0800

    Update sensitive_words_filter.py

--- sensitive_words_filter.py
+++ sensitive_words_filter.py
@@ -9,10 +9,16 @@ def load_sensitive_words(file_path):
 # 定义一个函数来检测输入文本中是否包含敏感词
 def check_sensitive_words(text, sensitive_words):
     words = jieba.lcut(text)  # 适用中文的分词库
+    new_text = ""
+    flag=0
     for word in words:
         if word in sensitive_words:
-            return 1  # 发现敏感词，返回1
-    return 0  # 没有发现敏感词，返回0
+            new_text += '*' * len(word)  # 替换为相同长度的星号
+            flag=1
+        else:
+            new_text += word
+        new_text += " "  # 添加空格以分隔单词
+    return new_text.strip(), flag
 
 if __name__=='__main__':
     # 敏感词库文件路径
@@ -22,9 +28,9 @@ if __name__=='__main__':
     sensitive_words_list = load_sensitive_words(word_file_path)
 
     # 测试样例
-    test_text = "请输入内容"
+    test_text = "今天太阳很好，我想。"
 
     # 检测测试样例中的文本
-    result = check_sensitive_words(test_text, sensitive_words_list)
+    new_text, result = check_sensitive_words(test_text, sensitive_words_list)
 
-    print(result)  # 输出 1 或 0
+    print(new_text, result) 
